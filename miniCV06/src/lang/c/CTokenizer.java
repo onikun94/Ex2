476c4b4d9d7241da89e524c028232aebe7905998
Taiyo Yamaguchi <onikun94@gmail.com>
2020-11-29 11:25:02 +0900

    インデントの整形

--- miniCV06/src/lang/c/CTokenizer.java
+++ miniCV06/src/lang/c/CTokenizer.java
@@ -8,116 +8,115 @@ import java.io.PrintStream;
 import lang.Tokenizer;
 
 public class CTokenizer extends Tokenizer<CToken, CParseContext> {
-	@SuppressWarnings("unused")
-	private CTokenRule	rule;
-	private int			lineNo, colNo;
-	private char		backCh;
-	private boolean		backChExist = false;
+    @SuppressWarnings("unused")
+    private CTokenRule	rule;
+    private int         lineNo, colNo;
+    private char        backCh;
+    private boolean     backChExist = false;
 
-	public CTokenizer(CTokenRule rule) {
-		this.rule = rule;
-		lineNo = 1; colNo = 1;
-	}
+    public CTokenizer(CTokenRule rule) {
+        this.rule = rule;
+        lineNo = 1; colNo = 1;
+    }
 
-	private InputStream in;
-	private PrintStream err;
+    private InputStream in;
+    private PrintStream err;
 
-	private char readChar() {
-		char ch;
-		if (backChExist) {
-			ch = backCh;
-			backChExist = false;
-		} else {
-			try {
-				ch = (char) in.read();
-			} catch (IOException e) {
-				e.printStackTrace(err);
-				ch = (char) -1;
-			}
-		}
-		++colNo;
-		if (ch == '\n')  { colNo = 1; ++lineNo; }
-//		System.out.print("'"+ch+"'("+(int)ch+")");
-		return ch;
-	}
-	private void backChar(char c) {
-		backCh = c;
-		backChExist = true;
-		--colNo;
-		if (c == '\n') { --lineNo; }
-	}
+    private char readChar() {
+        char ch;
+        if (backChExist) {
+            ch = backCh;
+            backChExist = false;
+        } else {
+            try {
+               ch = (char) in.read();
+            } catch (IOException e) {
+                e.printStackTrace(err);
+                ch = (char) -1;
+            }
+        }
+        ++colNo;
+        if (ch == '\n')  { colNo = 1; ++lineNo; }
 
-	// 現在読み込まれているトークンを返す
-	private CToken currentTk = null;
-	public CToken getCurrentToken(CParseContext pctx) {
-		return currentTk;
-	}
-	// 次のトークンを読んで返す
-	public CToken getNextToken(CParseContext pctx) {
-		in = pctx.getIOContext().getInStream();
-		err = pctx.getIOContext().getErrStream();
-		currentTk = readToken();
-//		System.out.println("Token='" + currentTk.toString());
-		return currentTk;
-	}
-	private CToken readToken() {
-		CToken tk = null;
-		char ch;
-		int  startCol = colNo;
-		StringBuffer text = new StringBuffer();
-		boolean isNum = false;
+        return ch;
+    }
+    private void backChar(char c) {
+        backCh = c;
+        backChExist = true;
+        --colNo;
+        if (c == '\n') { --lineNo; }
+    }
 
-		int state = 0;
-		boolean accept = false;
-		while (!accept) {
-			switch (state) {
-			case 0:					// 初期状態
-				ch = readChar();
-				//System.out.println("以下の文字で状態遷移"+ch);
-				if (ch == ' ' || ch == '\t' || ch == '\n' || ch == '\r') {
-				} else if (ch == (char) -1) {	// EOF
-					System.out.println("EOF処理に移動");
-					startCol = colNo - 1;
-					state = 1;
-				} else if (ch > '0' && ch <= '9') {
-					System.out.println("数字を扱う");
-					startCol = colNo - 1;
-					text.append(ch);
-					state = 3;
-				} else if(ch =='0') {
-					startCol = colNo - 1;
-					text.append(ch);
-					state = 10;
-				}
-				else if (ch == '+') {
-					startCol = colNo - 1;
-					text.append(ch);
-					state = 4;
-				} else if (ch == '-') {
-					startCol = colNo - 1;
-					text.append(ch);
-					state = 5;
-				} else if (ch == '/') {
-					startCol = colNo - 1;
-					text.append(ch);
-					state = 6;
-				} else if(ch == '&'){//
-					startCol = colNo -1;
-					text.append(ch);
-					state = 11;
-				} else if(ch == '*') {
+    // 現在読み込まれているトークンを返す
+    private CToken currentTk = null;
+    public CToken getCurrentToken(CParseContext pctx) {
+        return currentTk;
+    }
+    // 次のトークンを読んで返す
+    public CToken getNextToken(CParseContext pctx) {
+        in = pctx.getIOContext().getInStream();
+        err = pctx.getIOContext().getErrStream();
+        currentTk = readToken();
+
+        return currentTk;
+    }
+    private CToken readToken() {
+        CToken tk = null;
+        char ch;
+        int  startCol = colNo;
+        StringBuffer text = new StringBuffer();
+        boolean isNum = false;
+
+        int state = 0;
+        boolean accept = false;
+        while (!accept) {
+            switch (state) {
+            case 0:  // 初期状態
+                ch = readChar();
+                //System.out.println("以下の文字で状態遷移"+ch);
+                if (ch == ' ' || ch == '\t' || ch == '\n' || ch == '\r') {
+                } else if (ch == (char) -1) {	// EOF
+                    System.out.println("EOF処理に移動");
+                    startCol = colNo - 1;
+                    state = 1;
+                } else if (ch > '0' && ch <= '9') {
+                    System.out.println("数字を扱う");
+                    startCol = colNo - 1;
+                    text.append(ch);
+                    state = 3;
+                } else if(ch =='0') {
+                    startCol = colNo - 1;
+                    text.append(ch);
+                    state = 10;
+                } else if (ch == '+') {
+                    startCol = colNo - 1;
+                    text.append(ch);
+                    state = 4;
+                } else if (ch == '-') {
+                    startCol = colNo - 1;
+                    text.append(ch);
+                    state = 5;
+                } else if (ch == '/') {
+                    startCol = colNo - 1;
+                    text.append(ch);
+                    state = 6;
+                } else if(ch == '&'){//
+                    startCol = colNo -1;
+                    text.append(ch);
+                    state = 11;
+                } else if(ch == '*') {
                     startCol = colNo - 1;
                     text.append(ch);
                     state = 12;
-				} else if(ch == '(') {
+                } else if(ch == '(') {
                     startCol = colNo - 1;
                     text.append(ch);
                     state = 13;
-				} else if(ch == ')') {
+                } else if(ch == ')') {
                     startCol = colNo - 1;
                     text.append(ch);
                     state = 14;
-				}else if (ch == '[') {
+                }else if (ch == '[') {
                     startCol = colNo - 1;
                     text.append(ch);
                     state = 16;
