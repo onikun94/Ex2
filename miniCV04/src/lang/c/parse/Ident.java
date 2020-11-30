e1f2cf9bf58598d0dfef89cee76914b0caa1ff26
Taiyo Yamaguchi <onikun94@gmail.com>
2020-11-30 14:17:16 +0900

    チェックテスト前最終確認

--- miniCV04/src/lang/c/parse/Ident.java
+++ miniCV04/src/lang/c/parse/Ident.java
@@ -42,7 +42,7 @@ public class Ident extends CParseRule {
 
     	}else if(ident.getText().startsWith("ip_")) {
     		if(FactorAmp.isAddress == true) {
-    			pcx.fatalError("ポインタのアドレスは許しません");
+    			pcx.fatalError("ポインタのポインタは許しません");
     		}
     		this.setCType(CType.getCType(CType.T_pint));
     		System.out.println("ip_のときのT_pintの処理");
