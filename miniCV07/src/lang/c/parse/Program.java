27662dd42f56fd89cc42cbce810b9a50ba0a2152
Taiyo Yamaguchi <onikun94@gmail.com>
2021-01-07 21:57:08 +0900

    revise statementProcess in Program

--- miniCV07/src/lang/c/parse/Program.java
+++ miniCV07/src/lang/c/parse/Program.java
@@ -40,7 +40,12 @@ public class Program extends CParseRule {
 
 	public void semanticCheck(CParseContext pcx) throws FatalErrorException {
 		System.out.println("ProgramのsemanticCheck実行");
-		if (program != null) { program.semanticCheck(pcx); }
+		if (program != null) {
+			for(CParseRule i : statement){
+				System.out.println("------------------------------------------");
+				i.semanticCheck(pcx);
+			}
+		}
 	}
 
 	public void codeGen(CParseContext pcx) throws FatalErrorException {
@@ -53,8 +58,10 @@ public class Program extends CParseRule {
 		if (program != null) {
 			o.println("__START:");
 			o.println("\tMOV\t#0x1000, R6\t; ProgramNode: 計算用スタック初期化");
-			program.codeGen(pcx);
-			o.println("\tMOV\t-(R6), R0\t; ProgramNode: 計算結果確認用");
+			for(CParseRule i : statement) {
+				i.codeGen(pcx);
+			}
+			//o.println("\tMOV\t-(R6), R0\t; ProgramNode: 計算結果確認用");
 		}
 		o.println("\tHLT\t\t\t; ProgramNode:");
 		o.println("\t.END\t\t\t; ProgramNode:");
